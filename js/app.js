ec374530868a2fe43903b9d7490a4f3ce67d4689
Paul Gaudin <paulgaudin314@gmail.com>
2015-11-05 21:56:53 -0800

    why

--- js/app.js
+++ js/app.js
@@ -27,9 +27,4 @@
 
 skrollr.init({
         forceHeight: false,
-        easing: {
-            vibrate: function(p) {
-                return Math.sin(p * 10 * Math.PI);
-            }
-        }
-    });
+});
